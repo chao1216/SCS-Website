c63fb4d3c55cd292530a9ca7ab7ba8edd9ffc937
Daniel <evansdb0@sewanee.edu>
2016-12-01 03:23:07 -0600

    Improved install.sh with hints for fixes to potential installation problems

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 composer install
+RETVAL=$?
+if [ $RETVAL -ne 0]; then
+  echo "You need to install composer or add it to your PATH variable. Download Link: https://getcomposer.org/download/"
 cp .env.example .env
 php artisan key:generate
+npm install
+if [ $RETVAL -ne 0]; then
+  echo "You need to install npm/nodejs or add it to your PATH/configure the installation directory. Download Link: https://nodejs.org/en/download/
+
 
 printf "Don't forget to change your database config in .env file, and migrate
        the database tables with 'php artisan migrate'\n\n"
