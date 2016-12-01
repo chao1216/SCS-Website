b32b7f4e7d280a6d6fcabf77470b3951845fb85d
Daniel <evansdb0@sewanee.edu>
2016-12-01 03:29:11 -0600

    Further improvements to install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+check_status() {
+  if [ $1 -ne 0]; then
+    echo $2
+}
+
 composer install
-RETVAL=$?
-if [ $RETVAL -ne 0]; then
-  echo "You need to install composer or add it to your PATH variable. Download Link: https://getcomposer.org/download/"
+check_status $? "You need to install composer or add it to your PATH variable. Download Link: https://getcomposer.org/download/"
 cp .env.example .env
 php artisan key:generate
 npm install
-if [ $RETVAL -ne 0]; then
-  echo "You need to install npm/nodejs or add it to your PATH/configure the installation directory. Download Link: https://nodejs.org/en/download/
+check_status $? "You need to install npm/nodejs or add it to your PATH/configure the installation directory. Download Link: https://nodejs.org/en/download/"
+protractor
+check_status $? "You need to install protractor or you may need to install it globally with npm install -g protractor. See the readme file for more instructions."
 
 
 printf "Don't forget to change your database config in .env file, and migrate
